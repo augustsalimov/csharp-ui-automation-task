42745d185a38c7d9ac718d716cd8d5024bfda996
Guseyn Salimov <maximweller@protonmail.com>
2022-05-03 03:24:03 +0300

    fixes

--- RegionTests/RegionTests.cs
+++ RegionTests/RegionTests.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
 
 namespace RegionTests
 {
@@ -16,100 +16,105 @@ namespace RegionTests
         private readonly By _city_not_found_loc = By.CssSelector(".city-not-found");
         private const string _city_not_found_text = "Город не найден, возможно Вы имели ввиду:";
 
-        private readonly By _city_select_loc = By.XPath("//div[contains(@class,'city-select')]/div/p");
+        private readonly By _city_select_loc = By.XPath("//div[contains(@class, 'header-top')]/div[contains(@class,'city-select')]/div/p");
+
+        string[] _big_cities = { "Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург",
+            "Нижний Новгород", "Казань", "Самара", "Владивосток" };
+
+        private readonly By _region_loc = By.XPath("//ul[contains(@class, 'regions')]/li[contains(@style, 'display: block;')][2]");
+        private readonly By _city_loc = By.XPath("//ul[contains(@class, 'cities')]/li[contains(@style, 'display: block;')][2]");
+        string[] _regions = { "Дальневосточный", "Приволжский", "Северо-Западный", "Северо-Кавказский",
+            "Сибирский", "Уральский", "Центральный", "Южный" };
 
         [SetUp]
         public void Setup()
         {
-            //driver = new TWebDriver();
-            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.dns-shop.ru");
             driver.FindElement(By.CssSelector(".pull-right")).Click();
             Thread.Sleep(3000);
         }
 
         [Test]
-        public void Search_form()
+        public void Header_text()
         {
             driver.FindElement(By.XPath("//div[text()='Ваш город']"));
-
-            var search_form = driver.FindElement(_search_form_loc);
-
-            string input_text = "То";
-            search_form.SendKeys(input_text);
-            var input_text_in_cities = driver.FindElement(_to_in_cities_loc).Text;
-            Assert.AreEqual(input_text, input_text_in_cities, "Expected another text");
-
-            search_form.Clear();
-            search_form.SendKeys("Рандом");
-            var actual_text = driver.FindElement(_city_not_found_loc).Text;
-            Assert.AreEqual(_city_not_found_text, actual_text, "Expected another text");
         }
 
         [Test]
-        public void City_bubbles()
+        public void Search_form_prefix_of_city()
         {
-            var cities = new List<string>() { "Москва", "Санкт-Петербург", "Новосибирск",
-                "Екатеринбург", "Нижний Новгород", "Казань", "Самара", "Владивосток" };
+            var search_form = driver.FindElement(_search_form_loc);
+            string input_text = "То";
+            search_form.SendKeys(input_text);
+            var input_text_in_cities = driver.FindElement(_to_in_cities_loc).Text;
+            Assert.AreEqual(input_text, input_text_in_cities, $"Expected {input_text}, but it is {input_text_in_cities}");
+        }
 
-            foreach (var city in cities)
+        [Test]
+        public void Search_form_random_word()
+        {
+            var search_form = driver.FindElement(_search_form_loc);
+            search_form.SendKeys("Рандом");
+            var actual_text = driver.FindElement(_city_not_found_loc).Text;
+            Assert.AreEqual(_city_not_found_text, actual_text, $"Expected {_city_not_found_text}, but it is {actual_text}");
+        }
+
+        [Test]
+        public void Search_form_city()
+        {
+            var search_form = driver.FindElement(_search_form_loc);
+            string input_text = "Тольятти";
+            search_form.SendKeys(input_text);
+            driver.FindElement(By.XPath($"//mark[text()='{input_text}']")).Click();
+            Thread.Sleep(1000);
+            string actual_selected = driver.FindElement(_city_select_loc).GetDomProperty("innerText");
+            Assert.AreEqual(input_text, actual_selected, $"Expected {input_text}, but it is {actual_selected}");
+        }
+
+        [Test]
+        public void City_bubbles_check_items()
+        {
+            foreach (var city in _big_cities)
             {
                 var actual_city = driver.FindElement(By.XPath($"//a[text()='{city}']")).Text;
-                Assert.AreEqual(city, actual_city, "Expected another text");
+                Assert.AreEqual(city, actual_city, $"Expected {city}, but it is {actual_city}");
             }
+        }
 
+        [Test]
+        public void City_bubbles_click_random_bubble()
+        {
             Random rnd = new Random();
-            string random_city = cities[rnd.Next(cities.Count)];
+            string random_city = _big_cities[rnd.Next(_big_cities.Length)];
             driver.FindElement(By.XPath($"//a[text()='{random_city}']")).Click();
             Thread.Sleep(1000);
             string actual_selected = driver.FindElement(_city_select_loc).GetDomProperty("innerText");
-            Assert.AreEqual(random_city, actual_selected, "Expected another text");
+            Assert.AreEqual(random_city, actual_selected, $"Expected {random_city}, but it is {actual_selected}");
         }
 
         [Test]
         public void List_of_cities()
         {
-            driver.FindElement(By.XPath("//span[text()='Дальневосточный']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Амурская область']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Белогорск']"));
+            foreach (var region in _regions)
+            {
+                driver.FindElement(By.XPath($"//span[text()='{region}']")).Click();
+                driver.FindElement(_region_loc).Click();
+                var city = driver.FindElement(_city_loc);
+                string city_name = city.Text;
+                city.Click();
 
-            driver.FindElement(By.XPath("//span[text()='Приволжский']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Башкортостан']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Агиртамак']"));
-
-            driver.FindElement(By.XPath("//span[text()='Северо-Западный']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Город Санкт-Петербург']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Санкт-Петербург']"));
-
-            driver.FindElement(By.XPath("//span[text()='Северо-Кавказский']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Ингушетия']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Магас']"));
-
-            driver.FindElement(By.XPath("//span[text()='Сибирский']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Алтай']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Онгудай']"));
-
-            driver.FindElement(By.XPath("//span[text()='Уральский']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Тюменская область']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Тюмень']"));
-
-            driver.FindElement(By.XPath("//span[text()='Центральный']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Город Москва']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Москва']"));
-
-            driver.FindElement(By.XPath("//span[text()='Южный']")).Click();
-            driver.FindElement(By.XPath("//span[text()='Краснодарский край']")).Click();
-            string agoy = "Агой";
-            driver.FindElement(By.XPath($"//span[text()='{agoy}']")).Click();
-            Thread.Sleep(1000);
-            string actual_selected = driver.FindElement(_city_select_loc).GetDomProperty("innerText");
-            Assert.AreEqual(agoy, actual_selected, "Expected another text");
+                var actual_city = driver.FindElement(_city_select_loc);
+                string actual_city_text = actual_city.GetDomProperty("innerText");
+                actual_city.Click();
+                
+                Assert.AreEqual(city_name, actual_city_text, $"Expected {city_name}, but it is {actual_city_text}");
+            }
         }
 
         [TearDown]
         public void Quitting()
         {
-            Console.WriteLine("Selenium webdriver quit");
             driver.Quit();
         }
     }
